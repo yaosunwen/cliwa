9306c0176988b363f034a241f9022b819b591a05
Sunwen Yao <yaosunwen@qq.com>
2018-04-02 23:51:41 +0800

    replace tab with spaces

--- public/javascripts/updater.js
+++ public/javascripts/updater.js
@@ -4,11 +4,11 @@ var cliwa = cliwa || {};
   // $("<style type='text/css'> .redbold{ color:#f00; font-weight:bold;} </style>").appendTo("head");
   // $('head').append('<style>.loading:after{}</style>');
 
-	function Updater(selector) {
-		this.selector = selector;
-	}
-	Updater.prototype = Object.create(Object.prototype);
-	Updater.prototype.constructor = Updater;
+  function Updater(selector) {
+    this.selector = selector;
+  }
+  Updater.prototype = Object.create(Object.prototype);
+  Updater.prototype.constructor = Updater;
   Updater.prototype.createCSS = function () {
     if ($('#__loading__css__').length > 0) {
       return;
@@ -34,79 +34,79 @@ var cliwa = cliwa || {};
     var container = $(this.selector);
     container.append('<table class="terminal"></table>');
   }
-	Updater.prototype.onDone = function(data) {}
-	Updater.prototype.onData = function(data) {}
+  Updater.prototype.onDone = function(data) {}
+  Updater.prototype.onData = function(data) {}
 
-	function CmdUpdater(selector) {
-		Updater.call(this, selector);
-	}
-	CmdUpdater.prototype = Object.create(Updater.prototype);
-	CmdUpdater.prototype.constructor = CmdUpdater;
-	CmdUpdater.prototype.onStart = function() {}
-	CmdUpdater.prototype.onDone = function() {}
-	CmdUpdater.prototype.onData = function(data) {
+  function CmdUpdater(selector) {
+    Updater.call(this, selector);
+  }
+  CmdUpdater.prototype = Object.create(Updater.prototype);
+  CmdUpdater.prototype.constructor = CmdUpdater;
+  CmdUpdater.prototype.onStart = function() {}
+  CmdUpdater.prototype.onDone = function() {}
+  CmdUpdater.prototype.onData = function(data) {
     data = JSON.parse(data);
-		var container = $(this.selector);
-		container.find('input[name=command]').val(data.command);
-		for (var i in data.arguments) {
-			container.find('input[name=arguments]').eq(i).val(data.arguments[i]);
-		}
-	}
+    var container = $(this.selector);
+    container.find('input[name=command]').val(data.command);
+    for (var i in data.arguments) {
+      container.find('input[name=arguments]').eq(i).val(data.arguments[i]);
+    }
+  }
 
-	function ResultUpdater(selector) {
-		Updater.call(this, selector);
-	}
-	ResultUpdater.prototype = Object.create(Updater.prototype);
-	ResultUpdater.prototype.constructor = ResultUpdater;
-	ResultUpdater.prototype.onStart = function() {
+  function ResultUpdater(selector) {
+    Updater.call(this, selector);
+  }
+  ResultUpdater.prototype = Object.create(Updater.prototype);
+  ResultUpdater.prototype.constructor = ResultUpdater;
+  ResultUpdater.prototype.onStart = function() {
     Object.getPrototypeOf(ResultUpdater.prototype).onStart.call(this);
     this.showLoadingState();
   }
-	ResultUpdater.prototype.onDone = function() {
+  ResultUpdater.prototype.onDone = function() {
     Object.getPrototypeOf(ResultUpdater.prototype).onDone.call(this);
     this.hideLoadingState();
   }
-	ResultUpdater.prototype.onData = function(data) {
+  ResultUpdater.prototype.onData = function(data) {
     console.log('result updater received ' + data);
     data = JSON.parse(data);
-		var container = $(this.selector);
+    var container = $(this.selector);
     if (data.code == 0) {
-		  container.find('table').append('<tr><td class="line alert alert-success">Success</td></tr>');
+      container.find('table').append('<tr><td class="line alert alert-success">Success</td></tr>');
     } else {
-		  container.find('table').append('<tr><td class="line alert alert-danger">Failure with ' + data.code + '</td></tr>');
+      container.find('table').append('<tr><td class="line alert alert-danger">Failure with ' + data.code + '</td></tr>');
     }
-	}
+  }
 
-	function StdoutUpdater(selector) {
-		Updater.call(this, selector);
+  function StdoutUpdater(selector) {
+    Updater.call(this, selector);
     this.lastLine = null;
-	}
-	StdoutUpdater.prototype = Object.create(Updater.prototype);
-	StdoutUpdater.prototype.constructor = StdoutUpdater;
-	StdoutUpdater.prototype.onData = function(data) {
-		var container = $(this.selector);
-		var startIndex = 0;
-		while (startIndex < data.length) {
-			var endIndex = data.indexOf('\n', startIndex);
-			if (endIndex == -1) {
+  }
+  StdoutUpdater.prototype = Object.create(Updater.prototype);
+  StdoutUpdater.prototype.constructor = StdoutUpdater;
+  StdoutUpdater.prototype.onData = function(data) {
+    var container = $(this.selector);
+    var startIndex = 0;
+    while (startIndex < data.length) {
+      var endIndex = data.indexOf('\n', startIndex);
+      if (endIndex == -1) {
         if (this.lastLine == null) {
           this.lastLine = data.slice(startIndex);
         } else {
           this.lastLine += data.slice(startIndex);
         }
         break;
-			}
-			var line = data.slice(startIndex, endIndex);
-			startIndex = endIndex + 1;
+      }
+      var line = data.slice(startIndex, endIndex);
+      startIndex = endIndex + 1;
 
       if (this.lastLine !== null) {
         line = this.lastLine + line;
         this.lastLine = null;
       }
 
-			container.find('table').append('<tr><td class="line">' + line + '</td></tr>');
-		}
-	}
+      container.find('table').append('<tr><td class="line">' + line + '</td></tr>');
+    }
+  }
   StdoutUpdater.prototype.onDone = function() {
     Object.getPrototypeOf(StdoutUpdater.prototype).onDone.call(this);
     if (this.lastLine !== null) {
@@ -116,14 +116,14 @@ var cliwa = cliwa || {};
     }
   }
 
-	function StderrUpdater(selector) {
-		StdoutUpdater.call(this, selector);
-	}
-	StderrUpdater.prototype = Object.create(StdoutUpdater.prototype);
-	StderrUpdater.prototype.constructor = StderrUpdater;
+  function StderrUpdater(selector) {
+    StdoutUpdater.call(this, selector);
+  }
+  StderrUpdater.prototype = Object.create(StdoutUpdater.prototype);
+  StderrUpdater.prototype.constructor = StderrUpdater;
 
-	cliwa.CmdUpdater = CmdUpdater;
-	cliwa.ResultUpdater = ResultUpdater;
-	cliwa.StdoutUpdater = StdoutUpdater;
-	cliwa.StderrUpdater = StderrUpdater;
+  cliwa.CmdUpdater = CmdUpdater;
+  cliwa.ResultUpdater = ResultUpdater;
+  cliwa.StdoutUpdater = StdoutUpdater;
+  cliwa.StderrUpdater = StderrUpdater;
 })(cliwa, jQuery);
